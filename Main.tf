160fa5293bdf46612aa213beec864f5853037ab1
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-11 01:16:22 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,1 +1,107 @@
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-vino1234"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
 
+# Use AWS Terraform provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# VPC
+
+resource "aws_vpc" "default" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = {
+       Name = "wp-pvc-tf"
+    }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = aws_vpc.default.id
+
+    tags = {
+       Name = "wp-igw-tf"
+    }
+}
+
+# Subnets
+
+resource "aws_subnet" "wp-public-tf" {
+    vpc_id            = aws_vpc.default.id
+    cidr_block        = var.public_subnet_cidr_block
+    availability_zone = "us-west-2a"
+
+    tags = {
+       Name = "wp-public-tf"
+    }
+}
+
+resource "aws_subnet" "wp-private-tf" {
+    vpc_id            = aws_vpc.default.id
+    cidr_block        = var.private_subnet_cidr_block
+    availability_zone = "us-west-2b"
+
+    tags = {
+       Name = "wp-private-tf"
+    }
+}
+
+# Route Tables
+
+resource "aws_route_table" "wp-rt-public-tf" {
+    vpc_id = aws_vpc.default.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.default.id
+    }
+
+    tags = {
+       Name = "wp-rt-public-tf"
+    }
+}
+
+resource "aws_route_table_association" "wp-public-tf" {
+    subnet_id = aws_subnet.wp-public-tf.id
+    route_table_id = aws_route_table.wp-rt-public-tf.id
+}
+
+# SG
+resource "aws_security_group" "wp-sg-tf" {
+  name        = "wp-instance-tf"
+  description = "Security group for EC2 Instances"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wp-sg-tf"
+  }
+}
