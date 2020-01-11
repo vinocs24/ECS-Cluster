fe30e1e1eda12c9a160c2f546e27caf034a4bbef
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-11 12:42:05 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -13,95 +13,155 @@ provider "aws" {
 }
 
 # VPC
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = "true"
 
-resource "aws_vpc" "default" {
-    cidr_block = var.vpc_cidr_block
-
-    tags = {
-       Name = "wp-pvc-tf"
-    }
+  tags = {
+    Name = "demo-vpc"
+  }
 }
 
 # Internet Gateway
+resource "aws_internet_gateway" "demo-vpc-internet-gateway" {
+  vpc_id = aws_vpc.demo-vpc.id
 
-resource "aws_internet_gateway" "default" {
-    vpc_id = aws_vpc.default.id
-
-    tags = {
-       Name = "wp-igw-tf"
-    }
+  tags = {
+    Name = "demo-vpc-internet-gateway"
+  }
 }
 
 # Subnets
-
-resource "aws_subnet" "wp-public-tf" {
-    vpc_id            = aws_vpc.default.id
-    cidr_block        = var.public_subnet_cidr_block
+#Public1
+resource "aws_subnet" "demo-vpc-subnet1" {
+    vpc_id     = aws_vpc.demo-vpc.id
+    cidr_block = var.public1_subnet_cidr_block
     availability_zone = "us-west-2a"
 
     tags = {
-       Name = "wp-public-tf"
+        Name = "demo-vpc-subnet"
     }
 }
 
-resource "aws_subnet" "wp-private-tf" {
-    vpc_id            = aws_vpc.default.id
-    cidr_block        = var.private_subnet_cidr_block
+
+#Public2
+resource "aws_subnet" "demo-vpc-subnet2" {
+    vpc_id     = aws_vpc.demo-vpc.id
+    cidr_block = var.public2_subnet_cidr_block
     availability_zone = "us-west-2b"
 
     tags = {
-       Name = "wp-private-tf"
+        Name = "demo-vpc-subnet"
     }
 }
 
-# Route Tables
 
-resource "aws_route_table" "wp-rt-public-tf" {
-    vpc_id = aws_vpc.default.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.default.id
-    }
+#Private
+resource "aws_subnet" "demo-vpc-subnet3" {
+    vpc_id     = aws_vpc.demo-vpc.id
+    cidr_block = ar.private_subnet_cidr_block
+    availability_zone = "us-west-2c"
 
     tags = {
-       Name = "wp-rt-public-tf"
+        Name = "demo-vpc-subnet"
     }
 }
 
-resource "aws_route_table_association" "wp-public-tf" {
-    subnet_id = aws_subnet.wp-public-tf.id
-    route_table_id = aws_route_table.wp-rt-public-tf.id
-}
 
-# SG
-resource "aws_security_group" "wp-sg-tf" {
-  name        = "wp-instance-tf"
-  description = "Security group for EC2 Instances"
-  vpc_id      = aws_vpc.default.id
+# Route Tables
+resource "aws_route_table" "demo-vpc-route-table" {
+  vpc_id = aws_vpc.demo-vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  route {
+    cidr_block = "10.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo-vpc-internet-gateway.id
   }
 
   tags = {
-    Name = "wp-sg-tf"
+    Name = "demo-vpc-route-table"
   }
 }
+
+resource "aws_route_table_association" "demo-vpc-route-table-association1" {
+  subnet_id      = aws_subnet.demo-vpc-subnet1.id
+  route_table_id = aws_route_table.demo-vpc-route-table.id
+}
+
+resource "aws_route_table_association" "demo-vpc-route-table-association2" {
+  subnet_id      = aws_subnet.demo-vpc-subnet2.id
+  route_table_id = aws_route_table.demo-vpc-route-table.id
+}
+
+#Network ACL
+resource "aws_network_acl" "demo-vpc-network-acl" {
+    vpc_id = aws_vpc.demo-vpc.id
+    subnet_ids = [aws_subnet.demo-vpc-subnet1.id, aws_subnet.demo-vpc-subnet2.id]
+
+    egress {
+        protocol   = "-1"
+        rule_no    = 100
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 0
+        to_port    = 0
+    }
+
+    ingress {
+        protocol   = "-1"
+        rule_no    = 100
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 0
+        to_port    = 0
+    }
+
+    tags = {
+        Name = "demo-vpc-network-acl"
+    }
+}
+
+# SG
+resource "aws_security_group" "demo-vpc-security-group" {
+    name        = "demo-vpc-security-group"
+    description = "Allow HTTP, HTTPS, and SSH"
+    vpc_id = aws_vpc.demo-vpc.id
+
+    // HTTP
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // HTTPS
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // SSH
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+  tags = {
+    Name = "demo-vpc-security-group"
+  }
+}
+
+
+#NAT
