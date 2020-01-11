3448a6826c2c88981aa9b012e6e66362e58c251d
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-11 12:19:12 +0530

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -10,17 +10,17 @@ variable "key_name" {
 
 variable "vpc_cidr_block" {
   description = "VPC network"
-  default     = "10.1.0.0/16"
+  default     = "10.10.0.0/16"
 }
 
 variable "public_subnet_cidr_block" {
   description = "Public Subnet"
-  default     = "10.1.1.0/24"
+  default     = "10.10.1.0/24"
 }
 
 variable "private_subnet_cidr_block" {
   description = "Private Subnet"
-  default     = "10.1.2.0/24"
+  default     = "10.10.2.0/24"
 }
 
 variable "availability_zones" {
