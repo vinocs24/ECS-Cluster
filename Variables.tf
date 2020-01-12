2f2fe54c59685c84ee7f09af504b876fb6d8c99a
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-12 12:09:53 +0530

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -32,3 +32,34 @@ variable "availability_zones" {
   description = "Availability Zones"
   default     = "us-west-2a,us-west-2b,us-west-2c"
 }
+
+variable "ecs_cluster_name" {
+    description = "The name of the Amazon ECS cluster."
+    default = "main"
+}
+
+variable "autoscale_min" {
+    default = "1"
+    description = "Minimum autoscale (number of EC2)"
+}
+
+variable "autoscale_max" {
+    default = "10"
+    description = "Maximum autoscale (number of EC2)"
+}
+
+variable "autoscale_desired" {
+    default = "4"
+    description = "Desired autoscale (number of EC2)"
+}
+
+variable "amis" {
+    description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
+    default = {
+        us-east-1 = "ami-ddc7b6b7"
+    }
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
