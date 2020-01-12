79bf3bd95cc80c0968672478c652e26770ad9092
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-12 12:27:31 +0530

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -33,6 +33,11 @@ variable "availability_zones" {
   default     = "us-west-2a,us-west-2b,us-west-2c"
 }
 
+variable "availability_zone" {
+    description = "The availability zone"
+    default = "us-west-2a"
+}
+
 variable "ecs_cluster_name" {
     description = "The name of the Amazon ECS cluster."
     default = "main"
