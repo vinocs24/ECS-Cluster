1555e52812ba90ecad9d4d2339a85c3d80386d11
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-12 12:16:36 +0530

    Update services.tf

--- services.tf
+++ services.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "test-http" {
-    name = test-http-elb
+    name = "test-http-elb"
     security_groups = [aws_security_group.demo-vpc-security-group.id]
     subnets = [aws_subnet.demo-vpc-subnet3.id]
 
@@ -33,7 +33,7 @@ resource "aws_ecs_service" "test-http" {
     task_definition = aws_ecs_task_definition.test-http.arn
     iam_role = aws_iam_role.ecs_service_role.arn
     desired_count = 2
-    depends_on = ["aws_iam_role_policy.ecs_service_role_policy"]
+    depends_on = [aws_iam_role_policy.ecs_service_role_policy]
 
     load_balancer {
         elb_name = aws_elb.test-http.id
